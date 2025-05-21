693d65b329277ef6974613a24ce8f0256cf89774
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-05-21 10:21:24 +0300

    начальный коммит

---
+++ src/test/java/com/skypro/recommender/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
