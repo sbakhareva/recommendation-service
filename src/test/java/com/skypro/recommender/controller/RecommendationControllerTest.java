aca8be51d32380350c709048d43381681d713e6b
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-05-25 15:17:49 +0300

    модульное тестирование

--- src/test/java/com/skypro/recommender/controller/RecommendationControllerTest.java
+++ src/test/java/com/skypro/recommender/controller/RecommendationControllerTest.java
@@ -1,15 +1,12 @@
 package com.skypro.recommender.controller;
 
 import com.skypro.recommender.model.dto.RecommendationDTO;
-import com.skypro.recommender.repository.RecommendationInfoRepository;
-import com.skypro.recommender.repository.RecommendationsRepository;
 import com.skypro.recommender.service.RecommendationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.UUID;
